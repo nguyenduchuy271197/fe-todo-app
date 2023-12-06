154fc2eea7be6fee6f78d8199c7772e22ac9e79c
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-06 11:07:19 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,15 @@ const todoListEl = document.getElementById("todo-list");
 const taskDoneFilterEl = document.getElementById("task-done-filter");
 const portalEl = document.getElementById("portal");
 
-// Check tasks empty
-checkTasksEmpty();
+// Tasks data
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Display all tasks
+document.addEventListener("DOMContentLoaded", () => {
+  tasks.forEach((task) => {
+    appendTask(task);
+  });
+});
 
 // Add new task to the list of tasks
 addTaskBtnEl.addEventListener("click", () => {
@@ -14,115 +21,24 @@ addTaskBtnEl.addEventListener("click", () => {
     alert("Please write a task");
   } else {
     const id = generateRandomId();
-    const editModalId = "edit-modal-" + id;
-    const editInputId = "edit-input-" + id;
 
-    // Generate markup for new task
-    const markup = `
-    <li class="todo-item" id="todo-${id}">
-      <button class="todo-btn-check" onclick="check('todo-${id}')">
-        <ion-icon name="checkmark-outline" class="check-icon"></ion-icon>
-      </button>
-      <label for="todo-${id}" class="todo-label">${task}</label>
-      <div class="btn-actions">
-        <button
-        class="btn btn-info"
-        data-bs-toggle="modal"
-        data-bs-target="#${editModalId}"
-        >
-          <ion-icon name="create-outline"></ion-icon>
-        </button>
-        <button class="btn btn-danger" onclick="deleteTask('todo-${id}')">
-          <ion-icon name="trash-outline"></ion-icon>
-        </button>
-      </div>
-    </li>
-    `;
+    // Generate the task object and push it to the list of tasks
+    const newTask = {
+      id: id,
+      label: task,
+      done: false,
+    };
+    tasks.push(newTask);
 
-    // Generate markup for edit task modal
-    const modal = `
-    <div
-    class="modal fade"
-    id="${editModalId}"
-    tabindex="-1"
-    aria-labelledby="${editModalId}"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="${editModalId}">Edit Task</h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <input
-            type="text"
-            class="form-control"
-            id="${editInputId}"
-            value="${task}"
-          />
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            onclick="updateTask('${editInputId}','todo-${id}', '${editModalId}')"
-          >
-            Save changes
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
+    // Save to the list of tasks to localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    // Add to the end of the list
-    todoListEl.insertAdjacentHTML("beforeend", markup);
-    portalEl.insertAdjacentHTML("beforeend", modal);
-
-    const modalEl = document.getElementById(editModalId);
-
-    // Add event on modal when it closes
-    modalEl.addEventListener("hidden.bs.modal", (event) => {
-      const editInputEl = document.getElementById(editInputId);
-      const taskLabel = document.querySelector(`#todo-${id} .todo-label`);
-
-      editInputEl.value = taskLabel.textContent;
-    });
-
-    // Add event on modal when it shows
-    modalEl.addEventListener("shown.bs.modal", (event) => {
-      const editInputEl = document.getElementById(editInputId);
-
-      editInputEl.select();
-    });
-
-    // Remove alert
-    checkTasksEmpty();
+    // Render task to DOM
+    appendTask(newTask);
 
     // Reset input
     addTaskInputEl.value = "";
     addTaskInputEl.focus();
-  }
-});
-
-taskDoneFilterEl.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    todoListEl.classList.add("only-done");
-  } else {
-    todoListEl.classList.remove("only-done");
   }
 });
 
@@ -132,9 +48,32 @@ function deleteTask(id) {
 
   if (taskEl) {
     taskEl.remove();
-    checkTasksEmpty();
+    tasks = tasks.filter((task) => "todo-" + task.id != id);
+    // Save to the list of tasks to localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   } else {
     alert("Task not found!");
+  }
+}
+
+// Update a task
+function updateTask(inputId, taskId, modalId) {
+  const inputEl = document.getElementById(inputId);
+  const taskEl = document.getElementById(taskId);
+  if (inputEl.value === "") {
+    alert("Write a task");
+  } else {
+    taskEl.querySelector(".todo-label").textContent = inputEl.value;
+    const task = tasks.find((task) => "todo-" + task.id == taskId);
+    task.label = inputEl.value;
+
+    // Save to the list of tasks to localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Close modal
+    const modalEl = document.getElementById(modalId);
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 
@@ -144,27 +83,127 @@ function check(id) {
   taskEl.classList.toggle("checked");
 }
 
-function updateTask(inputId, taskId, modalId) {
-  const inputEl = document.getElementById(inputId);
-  const taskEl = document.getElementById(taskId);
-  if (inputEl.value === "") {
-    alert("Write a task");
+// Filter task done
+taskDoneFilterEl.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    todoListEl.classList.add("only-done");
   } else {
-    taskEl.querySelector(".todo-label").textContent = inputEl.value;
-
-    // Close modal
-    const modalEl = document.getElementById(modalId);
-    const modal = bootstrap.Modal.getInstance(modalEl);
-    modal.hide();
+    todoListEl.classList.remove("only-done");
   }
+});
+
+// Generate a markup for task
+function generateTaskMarkup(task) {
+  const editModalId = "edit-modal-" + task.id;
+
+  // Generate markup for new task
+  const markup = `
+      <li class="todo-item" id="todo-${task.id}">
+        <button class="todo-btn-check" onclick="check('todo-${task.id}')">
+          <ion-icon name="checkmark-outline" class="check-icon"></ion-icon>
+        </button>
+        <label for="todo-${task.id}" class="todo-label">${task.label}</label>
+        <div class="btn-actions">
+          <button
+          class="btn btn-info"
+          data-bs-toggle="modal"
+          data-bs-target="#${editModalId}"
+          >
+            <ion-icon name="create-outline"></ion-icon>
+          </button>
+          <button class="btn btn-danger" onclick="deleteTask('todo-${task.id}')">
+            <ion-icon name="trash-outline"></ion-icon>
+          </button>
+        </div>
+      </li>
+      `;
+
+  return markup;
 }
 
-function checkTasksEmpty() {
-  const todoListEl = document.getElementById("todo-list");
-  const alertEl = document.getElementById("alert");
-  if (todoListEl.firstChild) {
-    alertEl.textContent = "";
-  } else {
-    alertEl.textContent = "No tasks found.";
-  }
+// Generate markup for modal
+function generateModalMarkup(task) {
+  const editModalId = "edit-modal-" + task.id;
+  const editInputId = "edit-input-" + task.id;
+  // Generate markup for edit task modal
+  const markup = `
+      <div
+      class="modal fade"
+      id="${editModalId}"
+      tabindex="-1"
+      aria-labelledby="${editModalId}"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="${editModalId}">Edit Task</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <input
+              type="text"
+              class="form-control"
+              id="${editInputId}"
+              value="${task.label}"
+            />
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              onclick="updateTask('${editInputId}','todo-${task.id}', '${editModalId}')"
+            >
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+
+  return markup;
+}
+
+function appendTask(task) {
+  const editInputId = "edit-input-" + task.id;
+  const editModalId = "edit-modal-" + task.id;
+
+  // Generate markup for new task
+  const taskMarkup = generateTaskMarkup(task);
+
+  // Generate markup for edit task modal
+  const modalMarkup = generateModalMarkup(task);
+
+  // Add to the end of the list
+  todoListEl.insertAdjacentHTML("beforeend", taskMarkup);
+  portalEl.insertAdjacentHTML("beforeend", modalMarkup);
+
+  // Add event on modal when it closes
+  const modalEl = document.getElementById(editModalId);
+  modalEl.addEventListener("hidden.bs.modal", (event) => {
+    const editInputEl = document.getElementById(editInputId);
+    const taskLabel = document.querySelector(`#todo-${task.id} .todo-label`);
+
+    editInputEl.value = taskLabel.textContent;
+  });
+
+  // Add event on modal when it shows
+  modalEl.addEventListener("shown.bs.modal", (event) => {
+    const editInputEl = document.getElementById(editInputId);
+
+    editInputEl.select();
+  });
 }
